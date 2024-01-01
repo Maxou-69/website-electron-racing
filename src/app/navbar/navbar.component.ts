cc1d2c314a38549004a57c64b6228844c44233a7
Maxou <96335174+Maxou-69@users.noreply.github.com>
2024-01-01 14:58:42 +0100

    working on commponent and module routing system

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
