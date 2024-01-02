e5ac9f04ae8043d0b90ba2ae0862c81768eb3745
Maxou <96335174+Maxou-69@users.noreply.github.com>
2024-01-02 23:20:01 +0100

    working on transition

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input
+} from '@angular/core';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-
+  // @Input() activePane: SlideType = 'bottom';
 }
+
+type SlideType = 'left' | 'right';
