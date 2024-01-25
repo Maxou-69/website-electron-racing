2ee0db44feb78619cb459850402bc97fe9e2009f
Maxou <96335174+Maxou-69@users.noreply.github.com>
2024-01-25 10:46:09 +0100

    new component

---
+++ src/app/process/process.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-process',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './process.component.html',
+  styleUrl: './process.component.css'
+})
+export class ProcessComponent {
+
+}
---
+++ src/app/sponsor/sponsor.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sponsor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sponsor.component.html',
+  styleUrl: './sponsor.component.css'
+})
+export class SponsorComponent {
+
+}
