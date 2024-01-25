6f59d9352876edf872a92b57a0ea48dce74e75bf
Maxou <96335174+Maxou-69@users.noreply.github.com>
2024-01-25 10:37:20 +0100

    route for new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'accueil', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
