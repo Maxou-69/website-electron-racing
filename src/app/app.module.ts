652d068a2b27f8fd1676dd0a16f07bf1f42c3347
Maxou <96335174+Maxou-69@users.noreply.github.com>
2024-03-19 18:32:10 +0100

    random fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NewsComponent } from './news/news.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -16,7 +17,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     AppComponent,
     HomeComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -8,7 +8,7 @@ import { ModalComponent } from './modal.component';
     <div class="news_main">
       <div *ngFor="let div of divcomponent; let i = index" class="div">
         <img class="img" [src]="div.picture" [alt]="div.picturealt">
-        <p class="news_paragraph">{{ div.paragraph }}</p>
+        <p class="news_paragraph">{{ div.content }}</p>
         <button class="modal_button" type="button" (click)="openModal(div)">Plus d'info</button>
       </div>
     </div>
@@ -20,13 +20,13 @@ export class NewsComponent {
     {
       title: 'Article 1',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      picture: "../../assets/logo_1.webp",
+      picture: "../../assets/",
       picturealt: 'Article 1',
     },
     {
       title: 'Article 2',
       content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      picture: '../../assets/logo_1.webp',
+      picture: '../../assets/',
       picturealt: 'Article 2',
     },
   ];
