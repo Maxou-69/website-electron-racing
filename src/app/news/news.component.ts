81aa8a2c00209eee98bcb4051d3b89817d989fba
Maxou <96335174+Maxou-69@users.noreply.github.com>
2024-03-23 19:52:40 +0100

    indentation is life, random fixe

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -4,15 +4,7 @@ import { ModalComponent } from './modal.component';
 
 @Component({
   selector: 'app-news',
-  template: `
-    <div class="news_main">
-      <div *ngFor="let div of divcomponent; let i = index" class="div">
-        <img class="img" [src]="div.picture" [alt]="div.picturealt">
-        <p class="news_paragraph">{{ div.content_text }}</p>
-        <button class="modal_button" type="button" (click)="openModal(div)">Plus d'info</button>
-      </div>
-    </div>
-  `,
+  templateUrl: './news.component.html',
   styleUrls: ['./news.component.css'],
 })
 export class NewsComponent {
@@ -20,38 +12,42 @@ export class NewsComponent {
     {
       title: 'Story Insta',
       content: '',
-      picture: "../../assets/",
+      picture: '../../assets/',
       picturealt: 'Article 1',
-      content_text: '',
+      content_text: 'Blabla bla bla article 1',
     },
     {
       title: 'Article 2',
-      content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      content:
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       picture: '../../assets/',
       picturealt: 'Article 2',
-      content_text: "Blablabla art2",
+      content_text: 'Blablabla art2',
     },
     {
       title: 'Article 3',
-      content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      content:
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       picture: '../../assets/',
       picturealt: 'Article 2',
-      content_text: "Blablabla art3",
+      content_text: 'Blablabla art3',
     },
     {
       title: 'Article 4',
-      content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      content:
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       picture: '../../assets/',
       picturealt: 'Article 2',
-      content_text: "Blablabla art4",
+      content_text: 'Blablabla art4',
     },
     {
       title: 'Article 5',
-      content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      content:
+        'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       picture: '../../assets/',
       picturealt: 'Article 2',
-      content_text: "Blablabla art5",
-    }
+      content_text: 'Blablabla art5',
+    },
   ];
 
   constructor(private dialog: MatDialog) {}
@@ -62,7 +58,7 @@ export class NewsComponent {
       data: data,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
     });
   }
